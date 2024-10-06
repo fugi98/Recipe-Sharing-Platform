f8f1dd218d0c6cfda49b9099c630070f50662543
Tahseen fathima <sawa.seido08@gmail.com>
2024-10-06 22:48:15 +0530

    Updated the ts file

--- src/services/recipeService.ts
+++ src/services/recipeService.ts
@@ -1,19 +1,19 @@
 // src/services/recipeService.ts
 import axios from 'axios';
 
-const API_URL = 'https://jsonplaceholder.typicode.com'; // Replace with real API URL
+const API_URL = 'https://6702c37bbd7c8c1ccd3fc81b.mockapi.io/recipes'; // Use your MockAPI endpoint
 
 export const getRecipes = async () => {
-  const response = await axios.get(`${API_URL}/recipes`);
+  const response = await axios.get(API_URL);
   return response.data;
 };
 
 export const getRecipeById = async (id: string) => {
-  const response = await axios.get(`${API_URL}/recipes/${id}`);
+  const response = await axios.get(`${API_URL}/${id}`);
   return response.data;
 };
 
 export const addRecipe = async (recipe: any) => {
-  const response = await axios.post(`${API_URL}/recipes`, recipe);
+  const response = await axios.post(API_URL, recipe);
   return response.data;
 };
