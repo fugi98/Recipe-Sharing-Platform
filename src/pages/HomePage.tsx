7d83574564754a0250d3adfc18293dc8653dbbad
Tahseen fathima <sawa.seido08@gmail.com>
2024-10-06 22:11:27 +0530

    changes to the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,29 @@
 // src/pages/HomePage.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Toolbar, Typography, Grid } from '@mui/material';
 import RecipeCard from '../components/RecipeCard';
+import { getRecipes } from '../services/recipeService';
 
 const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes().then(data => setRecipes(data));
+  }, []);
+
   return (
     <Container>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">
-            Recipe Sharing Platform
-          </Typography>
+          <Typography variant="h6">Recipe Sharing Platform</Typography>
         </Toolbar>
       </AppBar>
       <Grid container spacing={2} sx={{ mt: 2 }}>
-        <Grid item xs={12} sm={6} md={4}>
-          <RecipeCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <RecipeCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <RecipeCard />
-        </Grid>
+        {recipes.map((recipe: any) => (
+          <Grid item xs={12} sm={6} md={4} key={recipe.id}>
+            <RecipeCard recipe={recipe} />
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
